0952a61487e83b2fad28116a3338d1708bada581
astiwet <astiwet@mail.ru>
2026-05-14 21:06:06 +0300

    nfjsfj

--- test/anastasia.py
+++ test/anastasia.py
@@ -2,6 +2,7 @@ import requests
 
 
 def test_add_edit():
+    """ тест на создание и изменение с проверкой по id"""
     body = {"title": "generated", "completed": False}
     response = requests.post("https://todo-app-sky.herokuapp.com/", json=body)
     id = response.json()["id"]
