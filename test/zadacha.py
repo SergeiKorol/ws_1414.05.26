2a7981bcc1f0da6a0389b60e311ba21f5b105990
Евгений-Р-т <ewew240570@gmail.com>
2026-05-14 20:59:53 +0300

    123

--- test/zadacha.py
+++ test/zadacha.py
@@ -1,6 +1,6 @@
 import requests
 def test_cannot_create_completed_task():
-
+    """тест о котлетах"""
     body = {"title": "completed task", "completed": True}
     response = requests.post("https://todo-app-sky.herokuapp.com/", json=body)
 
